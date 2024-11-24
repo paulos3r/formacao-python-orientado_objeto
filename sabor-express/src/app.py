8cce23663df1a585ed882fe28afe3338f1bcec1a
Paulo Sergio <036paulo@gmail.com>
2024-11-24 17:44:28 -0300

    Adicionado logica de programacao para cadastrar, listar e sair

--- sabor-express/src/app.py
+++ sabor-express/src/app.py
@@ -1,25 +1,66 @@
 import os
 
+restaurants=['pizza','suchi']
+
 def display_options():
   print('1 CADASTRAR RESTAURANTE')
   print('2 LISTAR RESTAURANTE')
   print('3 SAIR \n')
 
-def cloose_options():
-  options = int( input('Escolha opção: ') )
+def options_invalid():
+  input('Escolha opção valida: ')
+  main()
 
-  if options==1:
-    print('Cadastrar restaurante')
-  elif options==2:
-    print('Listar restaurante')
-  else:
-    end_app()
+def return_main_manu():
+  main()
+
+def display_subtitle(texto):
+  os.system('clear')
+  print(texto)
+  print()
+
+def register_new_restaurant():
+  
+  display_subtitle('Cadastrar novo restorante')
+  name_restaurant = input('\nDigite nome do restorante: ')
+  restaurants.append(name_restaurant)
+  print(f'\no restourante foi cadastrado {name_restaurant}')
+
+  input('\ndigite uma tecla para retoranar menu principal: ')
+  main()
+
+def list_restaurant():
+  display_subtitle('Lista de restaurantes')
+
+  for restaurant in restaurants:
+    print(f'\nRestaurante {restaurant}')
+
+  input('\ndigite uma tecla para retoranar menu principal: ')
+  main()
+
+
+
+def cloose_options():
+  try:
+    options = int(input('Escolha opção: '))
+    if options==1:
+      register_new_restaurant()
+    elif options==2:
+      list_restaurant()
+    elif options==3:
+      end_app()
+    else:
+      options_invalid()
+  except:
+    print('variavem inadequada')
+    options_invalid()
+
 
 def end_app():
-  os.system('clear')
-  print('Sair')
+  display_subtitle('Sair')
 
 def main():
+  display_subtitle('')
   display_options()
   cloose_options()
 
